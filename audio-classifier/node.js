73e203db7bfa784dfd8752aae68791ac1a667aea
Nick Kasten <kastentx@gmail.com>
2019-02-22 13:13:12 -0600

    better handling of improper input

--- audio-classifier/node.js
+++ audio-classifier/node.js
@@ -13,39 +13,58 @@ module.exports = function (RED) {
         var node = this;
 
         node.on('input', function (msg) {
-            var client = new lib.ModelAssetExchangeServer({ domain: this.service.host });
-
-
-            client.body = msg.payload;
+            var errorFlag = false;
+            var client;
+            if (this.service && this.service.host) {
+                client = new lib.ModelAssetExchangeServer({ domain: this.service.host });
+            } else {
+                node.error('Host in configuration node is not specified.', msg);
+                errorFlag = true;
+            }
+            if (!errorFlag) {
+                client.body = msg.payload;
+            }
 
             var result;
-            var errorFlag = false;
-            if (node.method === 'get_metadata') {
-                var parameters = [], nodeParam, nodeParamType;
-
-                result = client.get_metadata(parameters);
+            if (!errorFlag && node.method === 'get_metadata') {
+                var get_metadata_parameters = [];
+                var get_metadata_nodeParam;
+                var get_metadata_nodeParamType;
+                result = client.get_metadata(get_metadata_parameters);
             }
-            
-            if (node.method === 'predict') {
+            if (!errorFlag && node.method === 'predict') {
                 var parameters = [], nodeParam, nodeParamType;
 
                 nodeParam = node.predict_audio;
                 nodeParamType = node.predict_audioType;
-                parameters.audio = msg.payload; 
                 
+                if (typeof msg.payload === 'object') {
+                    parameters.audio = msg.payload;
+                } else {
+                    node.error('Unsupported type: \'' + (typeof msg.payload) + '\', ' + 'msg.payload must be JSON object or buffer.', msg);
+                    errorFlag = true;
+                }
+
                 result = client.predict(parameters);
             }
-            
+            if (!errorFlag && result === undefined) {
+                node.error('Method is not specified.', msg);
+                errorFlag = true;
+            }
             if (!errorFlag) {
                 node.status({ fill: "blue", shape: "dot", text: "ModelAssetExchangeServer.status.requesting" });
                 result.then(function (response) {
                     if (node.method === 'get_metadata') {
                         msg.payload = response.body;
                     }
-
-                    if (node.method === 'predict' && response.body !== null && response.body !== undefined) {
-                        msg.payload = response.body.predictions[0].label || "Detection Error";
+                    if (node.method === 'predict') {
+                        if (response.body !== null && response.body !== undefined) {
+                            msg.payload = response.body.predictions[0].label || "Detection Error";
+                        } else {
+                            msg.payload = null;
+                        }
                     }
+                    
                     node.send(msg);
                     node.status({});
                 }).catch(function (error) {
