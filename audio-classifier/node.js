6eb88d315fddec979d067ff9eb37c277c98fe0f4
Nick Kasten <kastentx@gmail.com>
2019-02-22 11:10:36 -0600

    corrected payload processing

--- audio-classifier/node.js
+++ audio-classifier/node.js
@@ -13,87 +13,44 @@ module.exports = function (RED) {
         var node = this;
 
         node.on('input', function (msg) {
-            var errorFlag = false;
-            var client;
-            if (this.service && this.service.host) {
-                client = new lib.ModelAssetExchangeServer({ domain: this.service.host });
-            } else {
-                node.error('Host in configuration node is not specified.', msg);
-                errorFlag = true;
-            }
-            if (!errorFlag) {
-                client.body = msg.payload;
-            }
+            var client = new lib.ModelAssetExchangeServer({ domain: this.service.host });
+
+
+            client.body = msg.payload;
 
             var result;
-            if (!errorFlag && node.method === 'get_metadata') {
-                var get_metadata_parameters = [];
-                var get_metadata_nodeParam;
-                var get_metadata_nodeParamType;
-                result = client.get_metadata(get_metadata_parameters);
-            }
-            if (!errorFlag && node.method === 'predict') {
-                var predict_parameters = [];
-                var predict_nodeParam;
-                var predict_nodeParamType;
+            var errorFlag = false;
+            if (node.method === 'get_metadata') {
+                var parameters = [], nodeParam, nodeParamType;
 
-                if (typeof msg.payload === 'object') {
-                    predict_parameters.body = msg.payload;
-                } else {
-                    node.error('Unsupported type: \'' + (typeof msg.payload) + '\', ' + 'msg.payload must be JSON object or buffer.', msg);
-                    errorFlag = true;
-                }
-                                result = client.predict(predict_parameters);
+                result = client.get_metadata(parameters);
             }
-            if (!errorFlag && result === undefined) {
-                node.error('Method is not specified.', msg);
-                errorFlag = true;
+            
+            if (node.method === 'predict') {
+                var parameters = [], nodeParam, nodeParamType;
+
+                nodeParam = node.predict_audio;
+                nodeParamType = node.predict_audioType;
+                parameters.audio = msg.payload; 
+                
+                result = client.predict(parameters);
             }
-            var setData = function (msg, data) {
-                if (data) {
-                    if (data.response) {
-                        if (data.response.statusCode) {
-                            msg.statusCode = data.response.statusCode;
-                        }
-                        if (data.response.headers) {
-                            msg.headers = data.response.headers;
-                        }
-                        if (data.response.request && data.response.request.uri && data.response.request.uri.href) {
-                            msg.responseUrl = data.response.request.uri.href;
-                        }
-                    }
-                    if (data.body) {
-                        if (node.method === 'get_metadata') {
-                            msg.payload = data.body;
-                        }
-                        if (node.method === 'predict') {
-                            if (data.body.predictions && data.body.predictions.length > 0) {
-                                msg.payload = data.body.predictions[0].caption;
-                            } else {
-                                msg.payload = null;
-                            }
-                        }
-                        msg.details = data.body;
-                    }
-                }
-                return msg;
-            };
+            
             if (!errorFlag) {
-                node.status({ fill: 'blue', shape: 'dot', text: 'ModelAssetExchangeServer.status.requesting' });
-                result.then(function (data) {
-                    node.send(setData(msg, data));
+                node.status({ fill: "blue", shape: "dot", text: "ModelAssetExchangeServer.status.requesting" });
+                result.then(function (response) {
+                    if (node.method === 'get_metadata') {
+                        msg.payload = response.body;
+                    }
+
+                    if (node.method === 'predict' && response.body !== null && response.body !== undefined) {
+                        msg.payload = response.body.predictions[0].label || "Detection Error";
+                    }
+                    node.send(msg);
                     node.status({});
                 }).catch(function (error) {
-                    var message = null;
-                    if (error && error.body) {
-                        if (error.body.message) {
-                            message = error.body.message;
-                        } else {
-                            message = error.body;
-                        }
-                    }
-                    node.error(message, setData(msg, error));
-                    node.status({ fill: 'red', shape: 'ring', text: 'node-red:common.status.error' });
+                    node.error(error, msg);
+                    node.status({ fill: "red", shape: "ring", text: "node-red:common.status.error" });
                 });
             }
         });
