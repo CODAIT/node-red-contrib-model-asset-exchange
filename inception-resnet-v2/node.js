05e3bdd6f5842ae28cd309cfcaa900acc357fb8c
Patrick Titzler <ptitzler@us.ibm.com>
2019-02-26 16:21:23 -0800

    i19: support for inception resnet v2

---
+++ inception-resnet-v2/node.js
@@ -0,0 +1,121 @@
+"use strict";
+var lib = require('./lib.js');
+
+module.exports = function (RED) {
+    function ModelAssetExchangeServerNode(config) {
+        RED.nodes.createNode(this, config);
+        this.service = RED.nodes.getNode(config.service);
+        this.method = config.method;
+
+        this.predict_image = config.predict_image;
+        this.predict_imageType = config.predict_imageType || 'str';
+
+        var node = this;
+
+        node.on('input', function (msg) {
+            var errorFlag = false;
+            var client;
+            if (this.service && this.service.host) {
+                client = new lib.ModelAssetExchangeServer({ domain: this.service.host });
+            } else {
+                node.error('Host in configuration node is not specified.', msg);
+                errorFlag = true;
+            }
+            if (!errorFlag) {
+                client.body = msg.payload;
+            }
+
+            var result;
+            var errorFlag = false;
+
+            if (!errorFlag && node.method === 'get_metadata') {
+                var get_metadata_parameters = [];
+                result = client.get_metadata(get_metadata_parameters);
+            }
+
+            if (!errorFlag && node.method === 'predict') {
+
+                var predict_parameters = [];
+                
+                if (typeof msg.payload === 'object') {
+                    predict_parameters.image = msg.payload;
+                } else {
+                    node.error('Unsupported type: \'' + (typeof msg.payload) + '\', ' + 'msg.payload must be JSON object or buffer.', msg);
+                    errorFlag = true;
+                }
+
+                node.log('Submitting prediction request ' + JSON.stringify(predict_parameters));
+                result = client.predict(predict_parameters);
+
+            }
+
+            if (!errorFlag && result === undefined) {
+                node.error('Method is not specified.', msg);
+                errorFlag = true;
+            }
+
+            var setData = function (msg, data) {
+                if (data) {
+                    if (data.response) {
+                        if (data.response.statusCode) {
+                            msg.statusCode = data.response.statusCode;
+                        }
+                        if (data.response.headers) {
+                            msg.headers = data.response.headers;
+                        }
+                        if (data.response.request && data.response.request.uri && data.response.request.uri.href) {
+                            msg.responseUrl = data.response.request.uri.href;
+                        }
+                    }
+                    if (data.body) {
+                        if (node.method === 'get_metadata') {
+                            msg.payload = data.body;
+                        }
+                        if (node.method === 'predict') {
+                            if (data.body.predictions && data.body.predictions.length > 0) {
+                                msg.payload = data.body.predictions[0].label;
+                            } else {
+                                msg.payload = null;
+                            }
+                        }
+                        msg.details = data.body;
+                    }
+                }
+                return msg;
+            };
+
+            if (!errorFlag) {
+                node.status({ fill: 'blue', shape: 'dot', text: 'ModelAssetExchangeServer.status.requesting' });
+                result.then(function (data) {
+                    node.send(setData(msg, data));
+                    node.status({});
+                }).catch(function (error) {
+                    var message = null;
+                    if (error && error.body && error.body.message) {
+                        message = error.body.message;
+                    }
+                    else {
+                        message = error;
+                    }
+                    node.error(message, setData(msg, error));
+                    node.status({ fill: 'red', shape: 'ring', text: 'node-red:common.status.error' });
+                });
+            }
+
+        });
+    }
+
+    RED.nodes.registerType("inception-resnet-v2", ModelAssetExchangeServerNode);
+
+    function ModelAssetExchangeServerServiceNode(n) {
+        RED.nodes.createNode(this, n);
+        this.host = n.host;
+
+    }
+
+    RED.nodes.registerType("inception-resnet-v2-service", ModelAssetExchangeServerServiceNode, {
+        credentials: {
+            temp: { type: "text" }
+        }
+    });
+};
