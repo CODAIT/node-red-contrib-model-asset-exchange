8574d6598882a0cdd8321a0bcaa978192b32d0c4
Nick Kasten <kastentx@gmail.com>
2019-03-05 16:06:49 -0600

    added image-segmenter node

--- image-segmenter/node.js
+++ image-segmenter/node.js
@@ -14,19 +14,27 @@ module.exports = function (RED) {
 
         node.on('input', function (msg) {
             var client = new lib.ModelAssetExchangeServer({ domain: this.service.host });
+            var errorFlag = false;
 
-
-            client.body = msg.payload;
+            if (this.service && this.service.host) {
+                client = new lib.ModelAssetExchangeServer({ domain: this.service.host });
+            } else {
+                node.error('Host in configuration node is not specified.', msg);
+                errorFlag = true;
+            }
+            if (!errorFlag) {
+                client.body = msg.payload;
+            }
 
             var result;
-            var errorFlag = false;
-            if (node.method === 'get_metadata') {
+            
+            if (!errorFlag && node.method === 'get_metadata') {
                 var parameters = [], nodeParam, nodeParamType;
 
                 result = client.get_metadata(parameters);
             }
 
-            if (node.method === 'predict') {
+            if (!errorFlag && node.method === 'predict') {
                 var parameters = [], nodeParam, nodeParamType;
 
                 nodeParam = node.predict_image;
@@ -42,17 +50,57 @@ module.exports = function (RED) {
                 result = client.predict(parameters);
             }
 
+            if (!errorFlag && result === undefined) {
+                node.error('Method is not specified.', msg);
+                errorFlag = true;
+            }
+
+            var setData = function (msg, data) {
+                if (data) {
+                    if (data.response) {
+                        if (data.response.statusCode) {
+                            msg.statusCode = data.response.statusCode;
+                        }
+                        if (data.response.headers) {
+                            msg.headers = data.response.headers;
+                        }
+                        if (data.response.request && data.response.request.uri && data.response.request.uri.href) {
+                            msg.responseUrl = data.response.request.uri.href;
+                        }
+                    }
+                    if (data.body) {
+                        if (node.method === 'get_metadata') {
+                            msg.payload = data.body;
+                        }
+                        if (node.method === 'predict') {
+                            if (data.body !== null && data.body !== undefined) {
+                                msg.payload = data.body.seg_map || "Detection Error";
+                            } else {
+                                msg.payload = null;
+                            }
+                            msg.details = data.body;
+                        }
+                    }
+                }
+                return msg;
+            };
+
             if (!errorFlag) {
                 node.status({ fill: "blue", shape: "dot", text: "ModelAssetExchangeServer.status.requesting" });
-                result.then(function (response) {
-                    if (response.body !== null && response.body !== undefined) {
-                        msg.payload = response.body;
-                    }
-                    node.send(msg);
+                result.then(function (data) {
+                    node.send(setData(msg, data));
                     node.status({});
                 }).catch(function (error) {
-                    node.error(error, msg);
-                    node.status({ fill: "red", shape: "ring", text: "node-red:common.status.error" });
+                    var message = null;
+                    if (error && error.body) {
+                        if (error.body.message) {
+                            message = error.body.message;
+                        } else {
+                            message = error.body;
+                        }
+                    }
+                    node.error(message, setData(msg, error));
+                    node.status({ fill: 'red', shape: 'ring', text: 'node-red:common.status.error' });
                 });
             }
         });
