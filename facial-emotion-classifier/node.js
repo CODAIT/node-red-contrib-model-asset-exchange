15a0c3dbfa98db89fa9803fbce110f207f0e37d3
Nick Kasten <kastentx@gmail.com>
2019-03-19 16:12:09 -0500

    clean output & add emotion node to package.json

--- facial-emotion-classifier/node.js
+++ facial-emotion-classifier/node.js
@@ -13,46 +13,97 @@ module.exports = function (RED) {
         var node = this;
 
         node.on('input', function (msg) {
-            var client = new lib.MaxFacialEmotionClassifier({ domain: this.service.host });
-
-
-            client.body = msg.payload;
+            var errorFlag = false;
+            var client;
+            if (this.service && this.service.host) {
+                this.service.host = this.service.host.replace(/\/+$/, '');
+                client = new lib.MaxFacialEmotionClassifier({ domain: this.service.host });
+            } else {
+                node.error('Host in configuration node is not specified.', msg);
+                errorFlag = true;
+            }
+            if (!errorFlag) {
+                client.body = msg.payload;
+            }
 
             var result;
-            var errorFlag = false;
-            if (node.method === 'get_labels') {
-                var parameters = [], nodeParam, nodeParamType;
+            if (!errorFlag && node.method === 'get_labels') {
+                var get_labels_parameters = [];
 
-                result = client.get_labels(parameters);
+                result = client.get_labels(get_labels_parameters);
             }
+            if (!errorFlag && node.method === 'get_model_metadata_api') {
+                var get_metadata_parameters = [];
 
-            if (node.method === 'get_model_metadata_api') {
-                var parameters = [], nodeParam, nodeParamType;
-
-                result = client.get_model_metadata_api(parameters);
+                result = client.get_model_metadata_api(get_metadata_parameters);
             }
+            if (!errorFlag && node.method === 'predict') {
+                var predict_parameters = [];
 
-            if (node.method === 'predict') {
-                var parameters = [], nodeParam, nodeParamType;
-
-                nodeParam = node.predict_image;
-                nodeParamType = node.predict_imageType;
-                parameters.image = nodeParamType === 'str' ? nodeParam || '' : RED.util.getMessageProperty(msg, nodeParam);
+                if (typeof msg.payload === 'object') {
+                    predict_parameters.image = msg.payload;
+                } else {
+                    node.error('Unsupported type: \'' + (typeof msg.payload) + '\', ' + 'msg.payload must be JSON object or buffer.', msg);
+                    errorFlag = true;
+                }
                 
-                result = client.predict(parameters);
+                result = client.predict(predict_parameters);
             }
-
-            if (!errorFlag) {
-                node.status({ fill: "blue", shape: "dot", text: "MaxFacialEmotionClassifier.status.requesting" });
-                result.then(function (response) {
-                    if (response.body !== null && response.body !== undefined) {
-                        msg.payload = response.body;
+            if (!errorFlag && result === undefined) {
+                node.error('Method is not specified.', msg);
+                errorFlag = true;
+            }
+            var setData = function (msg, data) {
+                if (data) {
+                    if (data.response) {
+                        if (data.response.statusCode) {
+                            msg.statusCode = data.response.statusCode;
+                        }
+                        if (data.response.headers) {
+                            msg.headers = data.response.headers;
+                        }
+                        if (data.response.request && data.response.request.uri && data.response.request.uri.href) {
+                            msg.responseUrl = data.response.request.uri.href;
+                        }
                     }
-                    node.send(msg);
+                    if (data.body) {
+                        if (node.method === 'get_model_metadata_api') {
+                            msg.payload = data.body;
+                        }
+                        if (node.method === 'get_labels') {
+                            msg.payload = data.body;
+                        }
+                        if (node.method === 'predict') {
+                            if (data.body.predictions && data.body.predictions.length > 0) {
+                                const predictions = data.body.predictions.map(person => person.emotion_predictions[0].label);
+                                msg.payload = predictions[0];
+            
+                                if (data.body.predictions.length > 1) {
+                                    const predictMap = {};
+                                    predictions.forEach((prediction, i) => predictMap[`face_${i}`]=prediction);
+                                    msg.payload = predictMap;
+                                } 
+                            } else {
+                                msg.payload = null;
+                            }
+                        }
+                        msg.details = data.body;
+                    }
+                }
+                return msg;
+            };
+            if (!errorFlag) {
+                node.status({ fill: 'blue', shape: 'dot', text: 'ModelAssetExchangeServer.status.requesting' });
+                result.then(function (data) {
+                    node.send(setData(msg, data));
                     node.status({});
                 }).catch(function (error) {
-                    node.error(error, msg);
-                    node.status({ fill: "red", shape: "ring", text: "node-red:common.status.error" });
+                    var message = null;
+                    if (error && error.body && error.body.message) {
+                        message = error.body.message;
+                    }
+                    node.error(message, setData(msg, error));
+                    node.status({ fill: 'red', shape: 'ring', text: 'node-red:common.status.error' });
                 });
             }
         });
