83f203eaa1ded43ad445a4daba0536588e27739b
Nick Kasten <kastentx@gmail.com>
2019-05-09 22:22:24 -0500

    basic box and label utils

--- utils/index.js
+++ utils/index.js
@@ -1,61 +1,56 @@
 const Jimp = require('jimp');
 
-function rect(img, x1, y1, x2, y2, height, fill, hexcolor)
-{
-	var white = Jimp.rgbaToInt(255, 255, 255, 255);
-	var color = hexcolor;
-	if(fill)
-	{
-		if(x1 < x2)
-		{
-			for(var x = x1; x <= x2; x++)
-			{
-				for(var y = y1; y <= height + y2; y++)
-				{
-					img.setPixelColor(color, x, y);
-				}
-			}
-		}
-	}
-	if(! fill)
-	{
-		if(x1 < x2)
-		{
-			for(var x = x1; x <= x2; x++)
-			{
-				for(var y = y1; y <= height + y2; y++)
-				{
-					if(y == y1 || y == height || x == x1 || x == x2)
-					{
-						img.setPixelColor(color, x, y);
-					}
-					else
-					{
-						img.setPixelColor(white, x, y);
-					}
-				}
-			}
-		}
-		if(x1 == x2)
-			throw "Can't draw a rectangle on one point";
-		if(x1 > x2)
-		{
-			for(var x = x2; x <= x1; x++)
-			{
-				for(var y = y1; y <= height + y2; y++)
-				{
-					if(y == y1 || y == height || x == x1 || x == x2)
-					{
-						img.setPixelColor(color, x, y);
-					}
-					else
-					{
-						img.setPixelColor(white, x, y);
-					}
-				}
-			}
-		}
-	}
+const rect = (img, xMin, yMin, xMax, yMax, padSize, color) => 
+  drawRect(img, xMin, yMin, xMax, yMax, padSize, color, false);
+
+const rectFill = (img, xMin, yMin, xMax, yMax, padSize, color) => 
+  drawRect(img, xMin, yMin, xMax, yMax, padSize, color, true);
+
+const drawRect = (img, xMin, yMin, xMax, yMax, padSize, color, isFilled) => {
+  for (x of range(xMin, xMax)) {
+    for (y of range(yMin, yMax)) { 
+      if (withinRange(y, yMin, padSize) || withinRange(x, xMin, padSize) || 
+          withinRange(y, yMax, padSize) || withinRange(x, xMax, padSize)) {
+        img.setPixelColor(Jimp.cssColorToHex(color), x, y);
+      } else if (isFilled && (y <= (yMax + padSize) && x <= (xMax + padSize))) {
+        img.setPixelColor(Jimp.cssColorToHex(color), x, y);
+      }
+    }
+  }
 }
 
-module.exports = rect;
+const getScaledFont = (width, color) => {
+  if (width > 1400)
+    return color === 'black' ? Jimp.FONT_SANS_128_BLACK : Jimp.FONT_SANS_128_WHITE;
+  else if (width > 700)
+    return color === 'black' ? Jimp.FONT_SANS_32_BLACK : Jimp.FONT_SANS_32_WHITE;
+  else
+  return color === 'black' ? Jimp.FONT_SANS_16_BLACK : Jimp.FONT_SANS_16_WHITE;
+}
+
+const getPadSize = width => {
+  if (width > 1400)
+    return 7;
+  else if (width > 1000)
+    return 4;
+  else if (width > 700)
+    return 3;
+  else
+    return 2;
+}
+
+const withinRange = (i, line, range) =>
+  (line-range<=i)&&(i<=line+range);
+
+function* range(start, end) {
+  for (let i = start; i <= end; i++) {
+      yield i;
+  }
+}
+
+module.exports = {
+  rect, 
+  rectFill,
+  getScaledFont,
+  getPadSize
+};
